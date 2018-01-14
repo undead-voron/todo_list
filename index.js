d4a652088243c2c103ed23f60217c1d82a431f3c
Den <undeadvoron@gmail.com>
2018-01-15 04:50:30 +0700

    app registrated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
-AppRegistry.registerComponent('mern', () => App);
+AppRegistry.registerComponent('todo_list', () => App);
